607a5152b6a534a071398c88c0e00fdabfe24f06
Matovu Ronald <matovuronald997@gmail.com>
2019-01-07 20:58:04 +0300

    Initial Uprage Commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Passport\Passport;
+use App\Models\Course;
+use App\Policies\CoursePolicy;
+use App\Models\Lesson;
+use App\Policies\LessonPolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+        Course::class => CoursePolicy::class,
+        Lesson::class => LessonPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Passport::routes();
+
+        //
+    }
+}
